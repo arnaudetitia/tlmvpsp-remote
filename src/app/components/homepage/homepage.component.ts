8ae01fa92fc2e493876a16a8c3a0d81188609527
arnaudetitia <arnaud.etitia@gmail.com>
2026-09-05 18:13:30 +0200

    ajout dialog connexion joueur

--- src/app/components/homepage/homepage.component.ts
+++ src/app/components/homepage/homepage.component.ts
@@ -1,4 +1,8 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { MatDialog } from '@angular/material/dialog';
+import { ConnectJoueurDialogComponent } from './connect-joueur/connect-joueur-dialog.component';
+import { tap } from 'rxjs';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-homepage',
@@ -6,4 +10,24 @@ import { Component } from '@angular/core';
   templateUrl: './homepage.component.html',
   styleUrl: './homepage.component.scss',
 })
-export class HomepageComponent {}
+export class HomepageComponent {
+  connectJoueurDialog = inject(MatDialog);
+
+  constructor(private router: Router) {}
+
+  openConnectJoueurDialog() {
+    const connectJoueurDialog = this.connectJoueurDialog.open(ConnectJoueurDialogComponent, {
+      width: '90vw',
+      disableClose: false,
+    });
+
+    connectJoueurDialog
+      .afterClosed()
+      .pipe(
+        tap(() => {
+          this.router.navigate(['/play']);
+        }),
+      )
+      .subscribe();
+  }
+}
