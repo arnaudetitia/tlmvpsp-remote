4e92440dfff01cb82451871b1934278ef5fee63e
arnaudetitia <arnaud.etitia@gmail.com>
2026-09-06 19:18:15 +0200

    acces remote champion

--- src/app/components/homepage/homepage.component.ts
+++ src/app/components/homepage/homepage.component.ts
@@ -3,6 +3,7 @@ import { MatDialog } from '@angular/material/dialog';
 import { ConnectJoueurDialogComponent } from './connect-joueur/connect-joueur-dialog.component';
 import { tap } from 'rxjs';
 import { Router } from '@angular/router';
+import { LoginChampionDialogComponent } from './login-champion-dialog/login-champion-dialog.component';
 
 @Component({
   selector: 'app-homepage',
@@ -12,12 +13,13 @@ import { Router } from '@angular/router';
 })
 export class HomepageComponent {
   connectJoueurDialog = inject(MatDialog);
+  loginChampionDialog = inject(MatDialog);
 
   constructor(private router: Router) {}
 
   openConnectJoueurDialog() {
     const connectJoueurDialog = this.connectJoueurDialog.open(ConnectJoueurDialogComponent, {
-      width: '90vw',
+      width: '75vw',
       disableClose: false,
     });
 
@@ -26,6 +28,24 @@ export class HomepageComponent {
       .pipe(
         tap(() => {
           this.router.navigate(['/play']);
+        }),
+      )
+      .subscribe();
+  }
+
+  openLoginChampionDialog() {
+    const loginChampionDialog = this.loginChampionDialog.open(LoginChampionDialogComponent, {
+      width: '75vw',
+      disableClose: false,
+    });
+
+    loginChampionDialog
+      .afterClosed()
+      .pipe(
+        tap((success) => {
+          if (success) {
+            this.router.navigate(['/champion']);
+          }
         }),
       )
       .subscribe();
--- src/app/models/enums/remote-storage-keys.enum.ts
+++ src/app/models/enums/remote-storage-keys.enum.ts
@@ -1,4 +1,5 @@
 export enum RemoteStorageKey {
   NOM_JOUEUR = 'nom_joueur',
   DATE_EXPIRATION = 'date_expiration',
+  CHAMPION_AUTH = 'champion_auth',
 }
